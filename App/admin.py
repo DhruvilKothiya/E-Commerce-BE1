3d765a3fb03b48530f593354f0cfedfced7fa3a9
DhruvilKothiya <dhruvil.kothiya@fxis.ai>
2025-01-06 11:04:42 +0530

    register image table in admin file

--- App/admin.py
+++ App/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Products,Categories,Wishlist,User
+from .models import Products,Categories,Wishlist,User,Images
 # Register your models here.
 admin.site.register(Products)
 admin.site.register(Categories)
 admin.site.register(Wishlist)
 admin.site.register(User)
+admin.site.register(Images)
